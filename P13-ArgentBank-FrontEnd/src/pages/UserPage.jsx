2bd88e4f676704f71206a243e08688dfdcc42b82
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-08-06 12:09:16 +0200

    userPage appelle maintenant le composant AccountPreview

--- P13-ArgentBank-FrontEnd/src/pages/UserPage.jsx
+++ P13-ArgentBank-FrontEnd/src/pages/UserPage.jsx
@@ -2,6 +2,8 @@ import classes from '/src/styles/UserPage.module.css';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
 import { Navigate } from 'react-router-dom';
 import Greetings from '../components/Greetings';
+import { getAccounts } from '../utils/getAccountsDetails';
+import AccountPreview from '../components/AccountPreview';
 
 function UserPage() {
 
@@ -11,6 +13,8 @@ function UserPage() {
         return <Navigate to={'/'} />;
     }
 
+    const accounts = getAccounts();
+    console.log(accounts);
     const pageTitle = `${userProfile.firstName} ${userProfile.lastName}`;
     document.title = `Argent Bank - ${pageTitle}`;
 
@@ -18,36 +22,9 @@ function UserPage() {
         <main className={`${classes.main} ${classes.bg_dark}`}>
             <Greetings firstName={userProfile.firstName} lastName={userProfile.lastName}/>
             <h2 className={classes.sr_only}>Accounts</h2>
-            <section className={classes.account}>
-                <div className={classes.account_content_wrapper}>
-                    <h3 className={classes.account_title}>Argent Bank Checking (x8349)</h3>
-                    <p className={classes.account_amount}>$2,082.79</p>
-                    <p className={classes.account_amount_description}>Available Balance</p>
-                </div>
-                <div className={`${classes.account_content_wrapper} ${classes.cta}`}>
-                    <button className={classes.transaction_button}>View transactions</button>
-                </div>
-            </section>
-            <section className={classes.account}>
-                <div className={classes.account_content_wrapper}>
-                    <h3 className={classes.account_title}>Argent Bank Savings (x6712)</h3>
-                    <p className={classes.account_amount}>$10,928.42</p>
-                    <p className={classes.account_amount_description}>Available Balance</p>
-                </div>
-                <div className={`${classes.account_content_wrapper} ${classes.cta}`}>
-                    <button className={classes.transaction_button}>View transactions</button>
-                </div>
-            </section>
-            <section className={classes.account}>
-                <div className={classes.account_content_wrapper}>
-                    <h3 className={classes.account_title}>Argent Bank Credit Card (x8349)</h3>
-                    <p className={classes.account_amount}>$184.30</p>
-                    <p className={classes.account_amount_description}>Current Balance</p>
-                </div>
-                <div className={`${classes.account_content_wrapper} ${classes.cta}`}>
-                    <button className={classes.transaction_button}>View transactions</button>
-                </div>
-            </section>
+            {accounts.map((account) => {
+                return <AccountPreview accountType={account.accountType} accountNumber={account.accountNumber} currentBalance={account.currentBalance} key={account.accountId}/>
+            })}
         </main>
     );
 }
