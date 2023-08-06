950c174960e6332ef09cd57baca298b90f6715b5
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-08-06 12:14:05 +0200

    Le bouton est un lien, au lieu d'utiliser useNavigate

--- P13-ArgentBank-FrontEnd/src/components/AccountPreview.jsx
+++ P13-ArgentBank-FrontEnd/src/components/AccountPreview.jsx
@@ -1,9 +1,7 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import classes from '/src/styles/AccountPreview.module.css';
 
 function AccountPreview({ accountType, accountNumber, currentBalance, }) {
-
-    const navigate = useNavigate();
 
     return (
         <section className={classes.account}>
@@ -13,7 +11,9 @@ function AccountPreview({ accountType, accountNumber, currentBalance, }) {
                 <p className={classes.account_amount_description}>Available Balance</p>
             </div>
             <div className={`${classes.account_content_wrapper} ${classes.cta}`}>
-                <button className={classes.transaction_button} onClick={navigate(`/userpage/account/${accountNumber}`)}>View transactions</button>
+                <Link to={`/userpage/account/${accountNumber}`}>
+                    <button className={classes.transaction_button}>View transactions</button>
+                </Link>
             </div>
         </section>
     );
