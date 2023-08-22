eb98939e158f1aa8dbcd8bf2245c6aab951de964
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-08-22 14:43:24 +0200

    suppression de code inutile

--- P13-ArgentBank-FrontEnd/src/pages/SignIn.jsx
+++ P13-ArgentBank-FrontEnd/src/pages/SignIn.jsx
@@ -1,27 +1,7 @@
-// import { useNavigate } from 'react-router-dom';
 import SignInContent from '../components/SignInContent';
 import classes from '/src/styles/SignIn.module.css';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { useEffect } from 'react';
 
 function SignIn() {
-
-    // const navigate = useNavigate();
-    // const dispatch = useDispatch();
-    // const userProfile = useSelector((state) => state.userReducer);
-
-    // useEffect(() => {
-    //     const token = localStorage.getItem('token');
-    //     if (userProfile.token){
-    //         navigate('/userpage');
-    //     } else if (!userProfile.token && token){
-    //         const getToken = async () =>{
-    //             await dispatch({type: GET_TOKEN, payload: token})
-    //             navigate('/userpage');
-    //         }
-    //         getToken();
-    //     }
-    // });
 
     const pageTitle = `Sign in`;
     document.title = `Argent Bank - ${pageTitle}`;
