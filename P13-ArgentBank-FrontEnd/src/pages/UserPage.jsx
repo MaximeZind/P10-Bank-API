6efd5859a955c139c23170826d8b4acc59d6b03e
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-08-23 12:02:46 +0200

    ajout de commentaires

--- P13-ArgentBank-FrontEnd/src/pages/UserPage.jsx
+++ P13-ArgentBank-FrontEnd/src/pages/UserPage.jsx
@@ -11,16 +11,19 @@ import Accounts from '../components/Accounts';
 
 function UserPage() {
 
+    const accounts = getAccounts();
     const userToken = useSelector((state) => state.tokenReducer);
     const userProfile = useSelector((state) => state.userReducer);
     const dispatch = useDispatch();
     const localStorageToken = localStorage.getItem('token');
     const sessionStorageToken = sessionStorage.getItem('token');
 
+    // Si pas de token (utilisateur non connecté), on redirige l'utilisateur vers la page d'accueil
     if (userToken === null && localStorageToken === null && sessionStorageToken === null) {
         return <Navigate to={'/'} />;
     }
 
+    //Récupération du userProfile en utilisant le token
     useEffect(() => {
         const getProfile = async () => {
             if (userToken) {
@@ -36,7 +39,6 @@ function UserPage() {
         getProfile();
     }, [userToken, localStorageToken, sessionStorageToken]);
 
-    const accounts = getAccounts();
     const pageTitle = `${userProfile.firstName} ${userProfile.lastName}`;
     document.title = `Argent Bank - ${pageTitle}`;
 
